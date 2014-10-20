69bf6786b31c7ea7ea587bfbabb0031d25c0266d
Thibault Vigouroux <tvigouroux@octo.com>
2014-10-20 14:51:32 +0200

    Added mino deploy

---
+++ config/deploy.rb
@@ -0,0 +1,26 @@
+require 'mina/bundler'
+require 'mina/rails'
+require 'mina/git'
+require 'mina/rbenv'
+
+set :rbenv_path, '/usr/local/rbenv/bin/rbenv'
+set :domain, '127.0.0.1'
+set :port, '2200'
+set :user, 'blog'
+set :deploy_to, '/var/www/blog'
+set :repository, 'git@github.com:TeaBough/blog.git'
+set :branch, 'master'
+
+task :environment do
+  invoke :'rbenv:load'
+end
+
+desc "Deploys the current version to the server."
+task :deploy => :environment do
+  queue 'export PATH=$PATH:/usr/local/rbenv/bin:/usr/local/rbenv/shims'
+  deploy do
+    invoke :'git:clone'
+    invoke :'bundle:install'
+    queue "#{bundle_prefix} jekyll build"
+  end
+end
